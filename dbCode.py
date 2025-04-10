faa11a7daf3b303d7b253e384fa9b1d0139acc4d
Maia Bentley <maiabentley@Maias-Air-2.drake.edu>
2025-04-10 15:25:54 -0500

    initial commit

--- dbCode.py
+++ dbCode.py
@@ -1,16 +1,48 @@
-import pymysql
-import creds 
 
-def mysqlconnect():
-    # To connect MySQL database
-    conn = pymysql.connect(
-        host= creds.host,
-        user= creds.user, 
-        password = creds.password,
-        db=creds.db,
-        )
-    cur = conn.cursor()
-    
-# Driver Code
-if __name__ == "__main__" :
-    mysqlconnect()
+import pymysql
+import creds
+
+def execute_query(query, args=()):
+    cur = get_conn().cursor()
+    cur.execute(query, args)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
+def get_conn():
+    return pymysql.connect(
+        host=creds.host,
+        user=creds.user,
+        password=creds.password,
+        db=creds.database,
+        cursorclass=pymysql.cursors.DictCursor  # So we can use column names in results
+    )
+
+def execute_query(query, args=None):
+    conn = get_conn()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(query, args or ())
+            result = cursor.fetchall()
+        return result
+    finally:
+        conn.close()
+
+def get_cities():
+    query = "SELECT Name, Population FROM City LIMIT 10;"
+    return execute_query(query)
+
+def get_languages():
+    query = "Select Distinct Language From countrylanguage Order By language;"
+    results = execute_query(query)
+    return [row['Language']for row in results]
+
+def get_list_of_dictionaries():
+    query = "SELECT Name, Population From country LIMIT 10;"
+    return execute_query(query)
+
+
+if __name__ == "__main__":
+    cities = get_cities()
+    for city in cities:
+        print(f"{city['Name']} - {city['Population']}")
--- flaskapp.py
+++ flaskapp.py
@@ -1,44 +1,94 @@
-from flask import Flask, render_template
-import mysql.connector
-import creds  # your credentials file
+from flask import Flask
 
 app = Flask(__name__)
+from flask import render_template
+import pymysql
+import creds 
 
-# Function to get DB connection
-def get_connection():
-    return mysql.connector.connect(
-        host=creds.host,
-        port=creds.port,
-        user=creds.user,
-        password=creds.password,
-        database=creds.database
-    )
+def get_conn():
+    conn = pymysql.connect(
+        host= creds.host,
+        user= creds.user, 
+        password = creds.password,
+        db=creds.database,
+        )
+    return conn
 
-# Route to home page
-@app.route('/')
+def execute_query(query, args=()):
+    cur = get_conn().cursor()
+    cur.execute(query, args)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
+
+#display the sqlite query in a html table
+def display_html(rows):
+    html = ""
+    html += """<table><tr><th>ArtistID</th><th>Artist</th><th>Track Title</th><th>Price</th><th>Milliseconds</th></tr>"""
+
+    for r in rows:
+        html += "<tr><td>" + str(r[0]) + "</td><td>" + str(r[1]) + "</td><td>" + str(r[2]) + "</td><td>" + str(r[3]) + "</td><td>" + str(r[4]) + "</td></tr>"
+    html += "</table></body>"
+    return html
+
+
+
+@app.route("/pricequery/<price>")
+def viewprices(price):
+    rows = execute_query("""select ArtistId, Artist.Name, Track.Name, UnitPrice, Milliseconds
+            from Artist JOIN Album using (ArtistID) JOIN Track using (AlbumID)
+            where UnitPrice = %s order by Track.Name 
+            Limit 500""", (str(price)))
+    return display_html(rows) 
+
+@app. route("/timequery/<time>")
+def viewtime(time):
+    rows = execute_query("""select ArtistId, Artist.Name, Track.Name, UnitPrice, Milliseconds
+            from Artist JOIN Album using (ArtistID) JOIN Track using (AlbumID)
+            where Milliseconds >= %s order by Track.Name 
+            """, (str(time)))
+    return display_html(rows) 
+
+from flask import request
+
+
+@app.route("/pricequerytextbox", methods = ['GET'])
+def price_form():
+  return render_template('textbox.html', fieldname = "Price")
+
+
+
+@app.route("/pricequerytextbox", methods = ['POST'])
+def price_form_post():
+  text = request.form['text']
+  return viewprices(text)
+
+@app.route("/timequerytextbox", methods = ['GET'])
+def time_form():
+    return render_template('textbox.html', fieldname = "Time")
+
+@app.route("/timequerytextbox", methods = ['POST'])
+def time_form_post():
+    text = request.form['text']
+    return viewtime(text)
+
+@app. route("/languages")
+def get_languages():
+    query = "Select Distinct Language From countrylanguage Order By language;"
+    results = execute_query(query)
+    return [row['Language']for row in results] 
+
+from dbCode import get_list_of_dictionaries
+@app.route('/cities')
 def home():
-    conn = get_connection()
-    cursor = conn.cursor(dictionary=True)
+    cities = get_cities()
+    return render_template('cities.html', cities=cities)
 
-    # Fetch categories
-    cursor.execute("SELECT * FROM Category")
-    categories = cursor.fetchall()
-
-    # Fetch inventory
-    cursor.execute("SELECT * FROM Inventory")
-    inventory = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    # Organize inventory by categoryID
-    grouped_inventory = {}
-    for cat in categories:
-        grouped_inventory[cat['categoryID']] = [
-            item for item in inventory if item['categoryID'] == cat['categoryID']
-        ]
-
-    return render_template('store.html', categories=categories, inventory=grouped_inventory)
+@app.route('/')
+def index():
+    countries = get_list_of_dictionaries()
+    return render_template("index.html", results=countries)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
